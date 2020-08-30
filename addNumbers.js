6a6703100ee3648ab5fde0b4634cbb90d32764db
Harika Paluri <harikapaluri@gmail.com>
2020-08-30 13:19:35 -0400

    first commit

---
+++ addNumbers.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express = require("express");
+const { body, validationResult } = require("express-validator");
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+var router = express.Router();
+
+// GET /questions
+// Route for questions collection
+router.post("/",
+[
+body("firstNumber").notEmpty().withMessage("Value is required").isInt().withMessage("Invalid Number value"),
+  body("secondNumber").notEmpty().withMessage("Value is required").isInt().withMessage("Invalid Number value"),
+  
+],
+ function(req, res){
+
+ 	//Validation check
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }else{
+   	var json={
+   		"success":true,
+   		"firstNumber":req.body.firstNumber,
+   		"secondNumber":req.body.secondNumber,
+        "result":req.body.firstNumber+req.body.secondNumber,  
+   	}
+   	res.json(json);
+   }
+	
+});
+
+
+module.exports = router;
